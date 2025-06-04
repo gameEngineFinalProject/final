58f9ed1b164db1ca9d0a865cc3b0ad06aa1f16c6
s1103344 <1013213amily@gmai.com>
2025-06-04 13:35:44 +0800

    add samu GameOverScene

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
-    [SerializeField] private GameObject gameOverPanel;
+    //[SerializeField] private GameObject gameOverPanel;
     //[SerializeField] private TextMeshProUGUI recipeDeliveredText;
     [SerializeField] private Button restartButton;
     [SerializeField] private Button mainMenuButton;
@@ -15,29 +15,62 @@ public class GameOverUI : MonoBehaviour
 
     private void Start()
     {
-        gameOverPanel.SetActive(false); // 開始時不顯示 Game Over 畫面
-        restartButton.onClick.AddListener(RestartGame);
-        mainMenuButton.onClick.AddListener(ReturnToMainMenu);
+        //gameOverPanel.SetActive(false); // 開始時不顯示 Game Over 畫面
+        //restartButton.onClick.AddListener(RestartGame);
+        //mainMenuButton.onClick.AddListener(ReturnToMainMenu);
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+        Hide();
+    }
+    private void Awake()
+    {
+        restartButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.GameScene);
+        });
+
+        mainMenuButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.MainMenu);
+        });
+        Time.timeScale = 1f;
     }
 
-    public void ShowGameOver(int recipesDelivered)
+    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e)
+    {
+        if (KitchenGameManager.Instance.isGameOver())
+        {
+            ShowGameOver();
+
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    public void ShowGameOver()
     {
         Time.timeScale = 0f; // 暫停遊戲
-        gameOverPanel.SetActive(true);
+        gameObject.SetActive(true);
         // recipeDeliveredText.text = "送出的食譜數量: " + recipesDelivered;
         scoreTextAtGameOver.text = "Final Score: " + ScoreManager.Instance.GetScore();
 
     }
 
-    private void RestartGame()
-    {
-        Time.timeScale = 1f; // 恢復時間
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+    //private void RestartGame()
+    //{
+    //    Time.timeScale = 1f; // 恢復時間
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //}
 
-    private void ReturnToMainMenu()
+    //private void ReturnToMainMenu()
+    //{
+    //    Time.timeScale = 1f; // 恢復時間
+    //    SceneManager.LoadScene("MainMenu"); // 確保你有一個名為 MainMenu 的場景
+    //}
+
+    private void Hide()
     {
-        Time.timeScale = 1f; // 恢復時間
-        SceneManager.LoadScene("MainMenu"); // 確保你有一個名為 MainMenu 的場景
+        gameObject.SetActive(false);
     }
 }
